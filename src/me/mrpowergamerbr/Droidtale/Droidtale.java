b22e9520f24ff2dfad1abbba4dc8e04ae73d37ee
MrPowerGamerBR <leonardomalaman@gmail.com>
2015-12-26 10:51:53 -0200

    I have no idea what I'm doing...
    But, the idea of trying to create a cool program so other people can use
    it.
    
    Fills you with DETERMINATION

---
+++ src/me/mrpowergamerbr/Droidtale/Droidtale.java
@@ -0,0 +1,43 @@
+package me.mrpowergamerbr.Droidtale;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Droidtale {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Droidtale window = new Droidtale();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Droidtale() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
